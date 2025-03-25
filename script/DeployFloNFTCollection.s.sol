6f3fcad93f44e7cd78446635aa245fbf86c9808a
Alberto <albertofm1992@gmail.com>
2025-03-25 21:50:39 +0200

    Initial functional version with 2 NFTs

---
+++ script/DeployFloNFTCollection.s.sol
@@ -0,0 +1,28 @@
+// 1. License
+//SPDX-License-Identifier: UNLICENSED
+
+//2. Solidity
+pragma solidity 0.8.28;
+
+//3. Contract
+
+import {Script} from "forge-std/Script.sol";
+import {FloNFTCollection} from "../src/FloNFTCollection.sol";
+
+contract DeployFloNFTCollection is Script{
+
+    function run() external returns(FloNFTCollection){
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        string memory name_="Flo Collection NFT";
+        string memory symbol_="FLO";
+        uint256 NFTCollectionSupply_=2;
+        string memory baseURI_="ipfs://bafybeidry2s3wemim3w7ki6zyngrnisr6ti5yykvuho4gy5yyyggywlm2y/";
+        FloNFTCollection floNFTCollection = new FloNFTCollection(name_,symbol_,NFTCollectionSupply_,baseURI_);
+        vm.stopBroadcast();
+        return floNFTCollection;
+
+    }
+
+    
+}
